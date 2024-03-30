2c645838a3cd10801aa5c8cc519c02d1415c55a0
saber-jabal <138504949+saber-jabal@users.noreply.github.com>
2024-03-30 12:51:02 +0000

    added subtraction to the Calculator class

--- Calculator.java
+++ Calculator.java
@@ -6,4 +6,8 @@ public class Calculator {
     public double addition(double a, double b) {
         return a + b;
     }
+
+    public int subtraction(int a , int b) {
+        return a - b;
+    }
 } 
