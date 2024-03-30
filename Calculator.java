e2afd1cf3852c2f9f5a63faa1a2f065e7eb9c6f1
saber-jabal <138504949+saber-jabal@users.noreply.github.com>
2024-03-30 13:47:43 +0000

    added double multiplication

--- Calculator.java
+++ Calculator.java
@@ -13,4 +13,7 @@ public class Calculator {
     public int subtraction(int a , int b) {
         return a - b;
     }
+    public double multiply(double a, double b) {
+        return a * b;
+    }
 } 
